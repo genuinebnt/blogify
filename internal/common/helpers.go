efc3658a5461cf7cf2ed7d1c3ab440658a99076e
genuinebasilnt <genuinebnt@gmail.com>
2025-05-26 10:12:52 +0530

    health check handler modifications

---
+++ internal/common/helpers.go
@@ -0,0 +1,24 @@
+package helpers
+
+import (
+	"encoding/json"
+	"maps"
+	"net/http"
+)
+
+func WriteJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	js = append(js, '\n')
+
+	maps.Copy(w.Header(), headers)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+
+	return nil
+}
--- internal/users/handler/healthcheck.go
+++ internal/users/handler/healthcheck.go
@@ -1,17 +1,35 @@
 package handler
 
-import "net/http"
+import (
+	"net/http"
 
-type HealthCheckHandler struct{}
+	helpers "github.com/genuinebnt/blogify/internal/common"
+	"github.com/genuinebnt/blogify/internal/common/config"
+)
 
-func NewHealthCheckHandler() *HealthCheckHandler {
-	return &HealthCheckHandler{}
+const version = "0.1.0"
+
+type HealthCheckHandler struct {
+	cfg *config.Config
+}
+
+func NewHealthCheckHandler(cfg *config.Config) *HealthCheckHandler {
+	return &HealthCheckHandler{
+		cfg,
+	}
 }
 
 func (h *HealthCheckHandler) CheckHealth() http.HandlerFunc {
+	data := map[string]string{
+		"status":      "available",
+		"environment": h.cfg.Env,
+		"version":     version,
+	}
+
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"status":"ok"}`))
+		err := helpers.WriteJSON(w, http.StatusOK, data, nil)
+		if err != nil {
+			http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		}
 	}
 }
